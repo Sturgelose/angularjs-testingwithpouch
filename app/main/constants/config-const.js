9510a6dcdc5087a6d2c070ff2ebd7b65822be92a
Erik Berdonces Bonelo <sturgelose@gmail.com>
2015-12-13 23:47:48 +0100

    Setup constants: callhome section URL and callhome country URL

--- app/main/constants/config-const.js
+++ app/main/constants/config-const.js
@@ -6,7 +6,8 @@ angular.module('main')
   // https://github.com/mwaylabs/generator-m-ionic#gulp-environment
   ENV: {
     /*inject-env*/
-    'SERVER_URL': 'https://DEVSERVER/api'
+    'CALLHOME_SECTION_URL': 'http://satellite.esn.org/callhome/api/section.json?mobile-api=1',
+    'CALLHOME_COUNTRY_URL': 'http://satellite.esn.org/callhome/api/country.json'
     /*endinject*/
   },
 
