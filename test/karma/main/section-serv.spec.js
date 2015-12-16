af1017228d5ddd3c1690f36974f2c6e10e3eaa13
Erik Berdonces Bonelo <sturgelose@gmail.com>
2015-12-16 11:30:03 +0100

    Update tests with comments from github (NOT WORKING)

--- test/karma/main/section-serv.spec.js
+++ test/karma/main/section-serv.spec.js
@@ -7,18 +7,52 @@ describe('module: main, service: Section', function () {
   // load all the templates to prevent unexpected $http requests from ui-router
   beforeEach(module('ngHtml2Js'));
 
-  // instantiate service
-  var sectionJson;
-  var Section;
-  beforeEach(inject(function (_Section_) {
-    Section = _Section_;
-    // Load fixtures
-    fixture.setBase('test/karma/fixtures')
-    sectionJson = fixture.load('json/section-list.json');
+  beforeEach(module(function ($provide) {
+    $provide.value('pouchDB', jasmine.createSpyObj('pouchDB', ['post']));
   }));
 
-  it('should do something', function () {
-    expect(!!Section).toBe(true);
+  var pouchDB;
+  var sectionJson;
+  var $httpBackend;
+  var $rootScope;
+  var Section;
+  var Config;
+  var $q;
+  beforeEach(inject(function ($injector) {
+    pouchDB = $injector.get('pouchDB');
+    pouchDB.post.and.returnValue($q.when('somedata'));
+    $httpBackend = $injector.get('$httpBackend');
+    $rootScope = $injector.get('$rootScope');
+    Section = $injector.get('Section');
+    Config = $injector.get('Config');
+    $q = $injector.get('$q');
+
+    // Load fixtures
+    fixture.setBase('test/karma/fixtures');
+    sectionJson = fixture.load('json/section-list.json');
+
+    $httpBackend
+      .expectGET(Config.ENV.CALLHOME_SECTION_URL).respond(200, sectionJson);
+  }));
+
+  describe('.getSection', function () {
+
+    it('should fetch all sections', function (done) {
+
+      pouchDB.post.and.returnValue($q.when('123'));
+      var sectionA = sectionJson[0];
+
+      Section.getSection('SectionA')
+        .then(function (answerSection) {
+          // Check the expects here....
+
+          // End the test
+          done();
+        });
+
+      $httpBackend.flush();
+    });
+
   });
 
 });
